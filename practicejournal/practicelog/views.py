e63ceeb2eda24639cd9c2c6c7b5287360e48eff6
bigdragon <blevinspercussion@gmail.com>
2024-01-30 15:00:01 -0500

    created view and template (placeholder file only) for practice screen

--- practicejournal/practicelog/views.py
+++ practicejournal/practicelog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def homeScreen(request):
     return render(request, "home.html")
+
+
+def practiceScreen(request):
+    return render(request, "practice.html")
