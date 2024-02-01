22ab20026c9468414900407f0a11b2b70e40ca26
bigdragon <blevinspercussion@gmail.com>
2024-01-31 19:21:59 -0500

    Added JS to toggle strikethrough on practice items

--- practicejournal/practicelog/static/JavaScript/script.js
+++ practicejournal/practicelog/static/JavaScript/script.js
@@ -1,1 +1,9 @@
-console.log("script loaded");
+let practiceItems = document.querySelectorAll(".practice-item");
+
+for (let i = 0; i < practiceItems.length; i++) {
+  practiceItems[i].addEventListener("click", () => {
+    practiceItems[i].classList.contains("completed")
+      ? practiceItems[i].classList.remove("completed")
+      : practiceItems[i].classList.add("completed");
+  });
+}
