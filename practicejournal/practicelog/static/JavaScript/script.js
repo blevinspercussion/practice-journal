105604d701082dc08ea20786c8e42c7f74233f81
Adam Blevins <blevinspercussion@gmail.com>
2024-02-11 09:12:41 -0500

    Added JS script code to make the menu work

--- practicejournal/practicelog/static/JavaScript/script.js
+++ practicejournal/practicelog/static/JavaScript/script.js
@@ -1,3 +1,18 @@
+// Handle main menu
+
+let navMenu = document.getElementById("nav-menu");
+let menuButton = document.getElementById("hamburger-div");
+
+menuButton.addEventListener("click", () => {
+  if (navMenu.style.visibility == "hidden") {
+    navMenu.style.visibility = "visible";
+  } else {
+    navMenu.style.visibility = "hidden";
+  }
+});
+
+// Handle Practice Page
+
 let practiceItems = document.querySelectorAll(".practice-item");
 
 for (let i = 0; i < practiceItems.length; i++) {
