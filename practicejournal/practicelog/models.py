2aa214b62f37efcf5c0bf6238948c01ee5f55d48
Adam Blevins <blevinspercussion@gmail.com>
2024-02-11 09:39:33 -0500

    Added ManyToManyField to User for Goal

--- practicejournal/practicelog/models.py
+++ practicejournal/practicelog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     firstName = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
-    # goals = models.ManyToManyField()
+    goals = models.ManyToManyField()
     minutesPracticed = models.IntegerField()
 
     def __str__(self):
