0738b2627a1a299e3b180d90f4887949f774bc6e
Adam Blevins <blevinspercussion@gmail.com>
2024-02-11 09:59:57 -0500

    Registered models in admin.py

--- practicejournal/practicelog/admin.py
+++ practicejournal/practicelog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Exercise, Goal, PracticeGoal
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Exercise)
+admin.site.register(Goal)
+admin.site.register(PracticeGoal)
